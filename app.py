5486f2cf91a1fe16d79dda1b8accefaff4d2ec10
Blizard_Wizard <BlizardWizard@users.noreply.github.com>
2020-03-01 01:07:19 -0800

    did something

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+import os
+import sys
 
 def main():
-
-    print("hang minorities")
+    if len(sys.argv) == 2:
+        print("video: " + sys.argv[1])
+    else:
+        print("please input a video link")
 
 main()
